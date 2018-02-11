1c0c93d2d276202733edfbd6dea40756a1d48926
Arnav Gupta <arnav@codingblocks.com>
2018-02-11 12:06:22 +0530

    intents + layouts
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture03/Layouts/app/src/main/java/com/codingblocks/layouts/MainActivity.java
@@ -0,0 +1,28 @@
+package com.codingblocks.layouts;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
