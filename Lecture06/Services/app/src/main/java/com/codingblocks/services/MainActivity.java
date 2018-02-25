6ded24bddaa786a7ad3b0c603eaa7a7b2983a1a5
Arnav Gupta <arnav@codingblocks.com>
2018-02-25 13:20:32 +0530

    onClick in Activity
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture06/Services/app/src/main/java/com/codingblocks/services/MainActivity.java
+++ Lecture06/Services/app/src/main/java/com/codingblocks/services/MainActivity.java
@@ -9,45 +9,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String TAG = "ACT";
     Button btnStartService, btnStopService, btnWait;
+    Intent srvIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Intent srvIntent = new Intent(MainActivity.this, MyService.class);
+        srvIntent = new Intent(MainActivity.this, MyService.class);
 
         btnStartService = findViewById(R.id.btnStartService);
         btnStopService = findViewById(R.id.btnStopService);
         btnWait = findViewById(R.id.btnWait);
 
 
-        btnStartService.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startService(srvIntent);
-            }
-        });
-
-        btnStopService.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stopService(srvIntent);
-
-            }
-        });
-
-        btnWait.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                waitNSec(10);
-//                Toast.makeText(MainActivity.this, "WAIT CLICKED", Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(MainActivity.this, MyIntentService.class);
-                startService(i);
-            }
-        });
+        btnStartService.setOnClickListener(this);
+        btnStopService.setOnClickListener(this);
+        btnWait.setOnClickListener(this);
     }
 
     static void waitNSec (int n) {
@@ -58,5 +38,21 @@ public class MainActivity extends AppCompatActivity {
     static void wait1Sec() {
         long startTime = System.currentTimeMillis();
         while (System.currentTimeMillis() < startTime + 1000);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnWait:
+                Intent i = new Intent(MainActivity.this, MyIntentService.class);
+                startService(i);
+                break;
+            case R.id.btnStartService:
+                startService(srvIntent);
+                break;
+            case R.id.btnStopService:
+                stopService(srvIntent);
+                break;
+        }
     }
 }
